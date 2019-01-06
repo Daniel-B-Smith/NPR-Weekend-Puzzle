ea526dc61cd92ced8d3d3afab838a4202e246a0e
Daniel Smith <daniel.smith@datadoghq.com>
2019-01-06 14:57:42 -0500

    Add unused benchmark

--- word_game_solver/benches/solver_bench.rs
+++ word_game_solver/benches/solver_bench.rs
@@ -15,5 +15,21 @@ fn bench_is_unique(c: &mut Criterion) {
     });
 }
 
+/*
+I'm keeping this benchmark around just in case, but commenting it out because it would require
+making the underlying function public. The function is pretty simple, and my one optimization idea
+(binary search) didn't help.
+
+fn bench_num_matches(c: &mut Criterion) {
+    c.bench_function("num_matches", |b| {
+        b.iter(|| {
+            num_matches(&['a', 'b', 'c', 'd', 'e'], &['a', 'c', 'e', 'f', 'g']);
+            num_matches(&['a', 'b', 'c', 'd', 'e'], &['a', 'c', 'f', 'g', 'h']);
+            num_matches(&['a', 'c', 'd', 'e', 'z'], &['a', 'b', 'e', 'f', 'g']);
+        })
+    });
+}
+*/
+
 criterion_group!(benches, bench_is_unique);
 criterion_main!(benches);
